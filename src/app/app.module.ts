db2821f1ed36db07cd8f7cc6414724886828f6c0
DamirLuketic <DamirLuketic>
2017-07-22 15:56:42 +0200

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopNaviComponent } from './components/layout/top-navi/top-navi.component';
+import { FooterComponent } from "./components/layout/footer/footer.component";
+import { HomeComponent } from './components/home/home.component';
+
+import { routes } from "./app.routes";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNaviComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      routes
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./components/home/home.component";
+
+const APP_ROUTES: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
+    {path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
+
+export const routes = RouterModule.forRoot(APP_ROUTES);
