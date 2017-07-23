37520cc7f850fc8842fd6634e6081249619f7c92
DamirLuketic <DamirLuketic>
2017-07-23 14:46:04 +0200

    Before test new HTTP client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,28 @@ import { FooterComponent } from "./components/layout/footer/footer.component";
 import { HomeComponent } from './components/home/home.component';
 
 import { routes } from "./app.routes";
+import { DropdownDirective } from './shared/directives/dropdown.directive';
+import { PersonalComponent } from './components/personal/personal.component';
+import { AlbumsComponent } from './components/albums/albums.component';
+import { UsersComponent } from './components/users/users.component';
+import { AccessComponent } from './components/access/access.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNaviComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    DropdownDirective,
+    PersonalComponent,
+    AlbumsComponent,
+    UsersComponent,
+    AccessComponent
   ],
   imports: [
     BrowserModule,
+      HttpClientModule,
       routes
   ],
   providers: [],
--- src/app/components/layout/top-navi/top-navi.component.ts
+++ src/app/components/layout/top-navi/top-navi.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-top-navi',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopNaviComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    @ViewChild('topSign')
+    'topSignElement': HTMLElement;
 
+    private color: string;
+
+
+      constructor() { }
+
+      ngOnInit() {
+          console.log(this.topSignElement);
+      }
+
+    openTab(value){
+
+        switch(value){
+            case 1:
+                this.color = 'red';
+                break;
+            case 2:
+                this.color = 'green';
+                break;
+            case 3:
+                this.color = 'blue';
+                break;
+            case 4:
+                this.color = 'orange';
+                break;
+            default:
+                this.color = 'red';
+        }
+
+    }
 }
