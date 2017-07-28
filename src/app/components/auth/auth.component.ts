c0367cd93464ba5f53d0417898d0f7ac457cf56f
DamirLuketic <luketic.damir@gmail.com>
2017-07-28 22:40:34 +0200

    Login update

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,24 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {LoginData} from "../../shared/class/login-data";
 import {AuthService} from "../../shared/services/auth.service";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
+
+    private loginSubscription: Subscription = null;
 
     constructor(private authService: AuthService) { }
 
     ngOnInit() {
         let loginData: LoginData = {nameEmail: 'luketic.damir@gmail.com', password: 'pass11' };
 
-        this.authService.logIn(loginData).subscribe(
+        this.loginSubscription = this.authService.logIn(loginData).subscribe(
             val => console.log(val),
             error => console.log(error),
             () => console.log('response compleat')
         )
+    }
+
+    ngOnDestroy(){
+        if(this.loginSubscription != null){
+            this.loginSubscription.unsubscribe();
+        }
     }
 
 }
