b7e2d0b6ce18caa7627c61d2b5f5bb89b6cce584
DamirLuketic <luketic.damir@gmail.com>
2017-07-29 12:35:43 +0200

    Update

--- src/app/components/personal/collection/collection.component.ts
+++ src/app/components/personal/collection/collection.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {AlbumService} from "../../../shared/services/album.service";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
   styleUrls: ['./collection.component.css']
 })
-export class CollectionComponent implements OnInit {
+export class CollectionComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private getCollectionSubscription: Subscription = null;
+
+  constructor(private albumService: AlbumService) { }
 
   ngOnInit() {
+    let id: number = 1;
+
+    this.getCollectionSubscription = this.albumService.getCollection(id).subscribe(
+        val => console.log(val),
+        error => console.log(error),
+        () => console.log('finish')
+    );
   }
 
+  ngOnDestroy(){
+    if(this.getCollectionSubscription != null){
+      this.getCollectionSubscription.unsubscribe();
+    }
+  }
 }
